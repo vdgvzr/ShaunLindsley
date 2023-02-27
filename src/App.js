100a481b16650c4b9faac8917fa471a2862d0848
Shaun Lindsley <shaunlindsley@gmail.com>
2023-02-27 18:55:03 +0000

    Import bootstrap and scss

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const App = () => {
-    return(
+    const [site, setSite] = useState(null);
+
+    useEffect(() => {
+        axios.get("/site.json").then((response) => {
+            console.log(response.data)
+            setSite(response.data);
+        }).catch((error) => {
+            console.error(error.message)
+        });
+    }, []);
+
+    return (
         <div>
-            Hello World!
+            Hello world!
         </div>
     )
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
-// import './scss/style.scss';
-// import './js/main.js'
+import './assets/scss/style.scss';
+import './assets/js/main.js'
 
 const container = document.getElementById('app');
 const root = createRoot(container);
